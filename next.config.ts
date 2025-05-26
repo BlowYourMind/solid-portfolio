9a114de46526006b9748d09c9bff26986470ffff
max <m.nikitins321@gmail.com>
2025-05-26 03:46:01 +0300

    Ignore eslint warnings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   plugins: {
     "@tailwindcss/postcss": {},
   },
